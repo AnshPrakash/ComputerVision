254bd2afbd8ca0108a3e012eae583751e177a4e9
AnshPrakash <anshprakash1997@gmail.com>
2019-11-09 02:14:51 +0530

    idk what it is doinggit add SimpleModel.py

--- Assignments/4/WhiteBackground/SimpleModel.py
+++ Assignments/4/WhiteBackground/SimpleModel.py
@@ -35,24 +35,27 @@ import matplotlib.pyplot as plt
 from torch.autograd import Variable
 from sklearn.metrics import f1_score
 
+
+
 cuda_avail = torch.cuda.is_available()
 
 def save_models(epoch):
-  torch.save(model.state_dict(), "./Models/SimpleMusicModel_{}.model".format(epoch))
+  torch.save(net.state_dict(), "./Models/SimpleMusicModel_{}.model".format(epoch))
   print("Chekcpoint saved :",epoch)
+
 
 class Net(nn.Module):
   def __init__(self):
     super(Net, self).__init__()
-    self.conv1 = nn.Conv2d(in_channels = 3,out_channels = 32, kernel_size = 3,stride =2,padding =(0,0))
-    self.bn2d_1 = nn.BatchNorm2d(32)
-    self.pool1 = nn.MaxPool2d(kernel_size=2,stride = 2)
-    self.conv2 = nn.Conv2d(in_channels = 32,out_channels = 64, kernel_size = 3,stride =2,padding =(0,0))
-    self.bn2d_2 = nn.BatchNorm2d(64)
-    self.pool2 = nn.MaxPool2d(kernel_size=2,stride = 2)
-    self.fc = nn.Linear(37440,2048)
-    self.bn1 = nn.BatchNorm1d(2048)
-    self.out_layer = nn.Linear(2048, 2)
+    self.conv1 = nn.Conv2d(in_channels = 3,out_channels = 16, kernel_size = 3,stride =2,padding =(0,0))
+    self.bn2d_1 = nn.BatchNorm2d(16)
+    self.pool1 = nn.MaxPool2d(kernel_size=2,stride = 1)
+    self.conv2 = nn.Conv2d(in_channels = 16,out_channels = 32, kernel_size = 3,stride =2,padding =(0,0))
+    self.bn2d_2 = nn.BatchNorm2d(32)
+    self.pool2 = nn.MaxPool2d(kernel_size=2,stride = 1)
+    self.fc = nn.Linear(3200,50)
+    self.bn1 = nn.BatchNorm1d(50)
+    self.out_layer = nn.Linear(50, 4)
     
   def forward(self, x):
     x = F.relu(self.conv1(x))
@@ -70,10 +73,11 @@ class Net(nn.Module):
     x = x.view(-1, self.num_flat_features(x))
     # print(x)
     x = F.relu(self.fc(x))
-    x = self.bn1(x)
+    # x = self.bn1(x)
     # print("After Hideen Layer",x)
     x = self.out_layer(x)
     # print("After Output layer",x)
+    #x = F.log_softmax(x, dim=1)
     x = F.softmax(x,dim=1)
     # print(x)
     return x
@@ -86,56 +90,50 @@ class Net(nn.Module):
     return num_features
 
 net = Net().cuda() if cuda_avail else Net()
+print(net)
 print("Cuda ",cuda_avail)
 
 # criterion = nn.NLLLoss()
-criterion = nn.BCELoss()
+# criterion = nn.BCELoss()
+# criterion = nn.CrossEntropyLoss()
+criterion = nn.MSELoss()
 # optimizer = optim.SGD(net.parameters(), lr=0.01,momentum = 0.9,weight_decay=1e-5)
 optimizer = torch.optim.Adam(net.parameters(), lr=0.001, eps=1e-8, amsgrad=False)
-epochs = 20
+epochs = 100
 batch_size = 32
 
-epochs = 1
-for epoch_i in range(4*epochs):
-  cap = cv2.VideoCapture(classes[epoch_i%4]+"train.mp4") 
-  if (cap.isOpened()== False):
-    print("Error opening video file")
-  while cap.isOpened():
-    ret, frame = cap.read() 
-    if ret == True:
-      frame = cv2.resize(frame, (50,50), interpolation = cv2.INTER_AREA)
-      cv2.imshow(classes[i%4]+"train.mp4", frame) 
-      if cv2.waitKey(25) & 0xFF == ord('q'): 
-        break
-    else:  
-      break
-  cap.release()
-  cv2.destroyAllWindows()
+Loss = []
+iterations = []
 
-
-epochs = 1
 for epoch_i in range(epochs):
   running_loss = 0.0
   total_pts = 0
   for label in labels:
-    cap = cv2.VideoCapture(classes[label]+"train.mp4") 
+    cap = cv2.VideoCapture(classes[label]+"train.mp4")
     if (cap.isOpened()== False):
       print("Error opening video file")
+    one_hot = [0,0,0,0]
+    one_hot[label] = 1
     while cap.isOpened():
       ret, frame = cap.read() 
       if ret == True:
         frame = cv2.resize(frame, (50,50), interpolation = cv2.INTER_AREA)
+        frame = frame/255.0
         if cuda_avail:
-          inputs = Variable(sample_batched['image'].cuda())
-          target = Variable(sample_batched['label'].cuda())
+          inputs = Variable(torch.from_numpy(frame.reshape(1,frame.shape[2],frame.shape[0],frame.shape[1]).float()).cuda())
+          target = Variable(torch.from_numpy(np.array([one_hot]).float()).cuda())
         else:                
-          inputs = Variable(sample_batched['image'])
-          target = Variable(sample_batched['label'])
+          inputs = Variable(torch.from_numpy(frame.reshape(1,frame.shape[2],frame.shape[0],frame.shape[1])).float())
+          target = Variable(torch.from_numpy(np.array([one_hot])).float())
+
         optimizer.zero_grad()   # zero the gradient buffers
         output = net(inputs)
         # print(output)
         # print(target)
         loss = criterion(output, target)
+        # loss = criterion(output)
+        # print(loss.grad_fn )
+        net.zero_grad()
         loss.backward()
         optimizer.step()    # Does the update
         running_loss += loss.cpu().item()
@@ -143,44 +141,19 @@ for epoch_i in range(epochs):
       else:  
         break
     cap.release()
+  save_models(epoch_i)
+  Loss.append(running_loss/total_pts)
+  iterations.append(epoch_i)
   print("Runnning Loss on whole DataSet on epoch ",epoch_i," is ",running_loss/total_pts)
+  
+
+
+plt.plot(iterations, Loss, 'r') # plotting t, a separately 
+# plt.plot(iterations, b, 'ValidLoss') # plotting t, b separately 
+plt.show()
 
 
 
-
-
-for epoch_i in range(epochs):
-  running_loss = 0.0
-  total_pts = 0
-  for i in range(1,501):
-    TrainingDataset = BreakOutDataset(file='TrainingData/'+str(i).zfill(8))
-    dataloader = DataLoader(TrainingDataset, batch_size=batch_size,shuffle=False, num_workers=4)
-    for i_batch, sample_batched in enumerate(dataloader):
-      if cuda_avail:
-        inputs = Variable(sample_batched['image'].cuda())
-        target = Variable(sample_batched['label'].cuda())
-      else:                
-        inputs = Variable(sample_batched['image'])
-        target = Variable(sample_batched['label'])
-      optimizer.zero_grad()   # zero the gradient buffers
-      if(inputs.size()[0] == 1):
-        break
-      output = net(inputs)
-      # print(output)
-      # print(target)
-      loss = criterion(output, target)
-      loss.backward()
-      optimizer.step()    # Does the update
-      running_loss += loss.cpu().item()
-      total_pts += len(target)
-      # print(torch.max(output,1)[-1],target)
-      # print("probabilties ",output)
-      # print(str(loss) +"\n")
-      # if i==250:
-      #   print("Running Loss After ",i,"episodes ",running_loss/total_pts)
-    # if (i+1)%2 == 0:
-    #     print("Accuracy ",accuracy('TrainingData/'+str(i).zfill(8)))
-  print("Runnning Loss on whole DataSet on epoch ",epoch_i," is ",running_loss/total_pts)
 
 
 
