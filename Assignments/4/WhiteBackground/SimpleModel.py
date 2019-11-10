4d081225f026c889e335e22be8b91e75ed9c8885
AnshPrakash <anshprakash1997@gmail.com>
2019-11-10 16:25:17 +0530

    added batch size

--- Assignments/4/WhiteBackground/SimpleModel.py
+++ Assignments/4/WhiteBackground/SimpleModel.py
@@ -31,7 +31,8 @@ labels = [0,1,2,3]
 from network import *
 
 
-cuda_avail = torch.cuda.is_available()
+# Device configuration
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 def save_models(epoch):
   torch.save(net.state_dict(), "./Models/SimpleMusicModel_{}.model".format(epoch))
@@ -39,9 +40,8 @@ def save_models(epoch):
 
 
 torch.manual_seed(0)
-net = Net().cuda() if cuda_avail else Net()
+net = Net().to(device)
 print(net)
-print("Cuda ",cuda_avail)
 
 # criterion = nn.NLLLoss()
 # criterion = nn.BCELoss()
@@ -50,7 +50,7 @@ criterion = nn.CrossEntropyLoss()
 # optimizer = optim.SGD(net.parameters(), lr=0.001,momentum = 0.9,weight_decay=1e-5)
 optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 epochs = 200
-
+batch_size = 32
 
 Loss = []
 iterations = []
@@ -65,27 +65,45 @@ for epoch_i in range(epochs):
     one_hot = [0,0,0,0]
     one_hot[label] = 1
     while cap.isOpened():
-      ret, frame = cap.read() 
-      if ret == True:
-        frame = cv2.split(frame)[0]
-        frame = cv2.resize(frame, (50,50), interpolation = cv2.INTER_AREA)
-        frame = frame/255.0
-        inputs = (torch.from_numpy(frame.reshape(1,1,frame.shape[0],frame.shape[1])).float())
-        target = Variable(torch.from_numpy(np.array([one_hot])).float())
-
-        optimizer.zero_grad()   # zero the gradient buffers
-        output = net(inputs)
-        # print(output)
-        # print(target)
-        # loss = criterion(output, target)
-        loss = criterion(output, torch.from_numpy(np.array([label]))) #for cross entropy
-        net.zero_grad()
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-        total_pts += len(target)
-      else:  
+      sizeOfBatch = 0
+      batch_data = []
+      batch_labels = []
+      while sizeOfBatch!=batch_size and cap.isOpened():
+        ret, frame = cap.read() 
+        if ret== True:
+          frame = cv2.split(frame)[0]
+          frame = cv2.resize(frame, (50,50), interpolation = cv2.INTER_AREA)
+          frame = frame/255.0 
+          batch_data.append(frame)
+          batch_labels.append(label)
+          sizeOfBatch += 1
+        else:
+          break  
+      if (len(batch_labels)) != batch_size:
         break
+      batch_data = np.array(batch_data)
+    
+      # ret, frame = cap.read() 
+      # frame = cv2.split(frame)[0]
+      # frame = cv2.resize(frame, (50,50), interpolation = cv2.INTER_AREA)
+      # frame = frame/255.0
+      # inputs = (torch.from_numpy(frame.reshape(batch_size,1,frame.shape[0],frame.shape[1])).float()).to(device)
+      # target = (torch.from_numpy(np.array([one_hot])).float()).to(device)
+
+      inputs = (torch.from_numpy(batch_data.reshape(batch_size,1,50,50)).float()).to(device)
+      output = net(inputs)
+      # print(output)
+      # print(output.size())
+      # print(batch_labels)
+      # print(target)
+      # loss = criterion(output, target)
+      loss = criterion(output, torch.from_numpy(np.array(batch_labels)).to(device)) #for cross entropy
+      optimizer.zero_grad()   # zero the gradient buffers
+      net.zero_grad()
+      loss.backward()
+      optimizer.step()
+      running_loss += loss.cpu().item()
+      total_pts += len(batch_labels)
     cap.release()
   save_models(epoch_i)
   Loss.append(running_loss/total_pts)
