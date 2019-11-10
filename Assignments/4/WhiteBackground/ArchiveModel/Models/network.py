8cde22f90b78f38ef9a91c766e38a06f0d0d2bf5
AnshPrakash <anshprakash1997@gmail.com>
2019-11-10 15:07:45 +0530

    trying another model

---
+++ Assignments/4/WhiteBackground/ArchiveModel/Models/network.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset,DataLoader
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
+from sklearn.metrics import f1_score
+
+
+# class Net(nn.Module):
+#   def __init__(self):
+#     super(Net, self).__init__()
+#     self.conv1 = nn.Conv2d(in_channels = 3,out_channels = 8, kernel_size = 3,stride =2,padding =(0,0))
+#     self.bn2d_1 = nn.BatchNorm2d(8)
+#     self.pool1 = nn.MaxPool2d(kernel_size=2,stride = 2)
+#     self.conv2 = nn.Conv2d(in_channels = 8,out_channels = 16, kernel_size = 3,stride =2,padding =(0,0))
+#     self.bn2d_2 = nn.BatchNorm2d(16)
+#     self.pool2 = nn.MaxPool2d(kernel_size=2,stride = 2)
+#     self.fc = nn.Linear(64,50)
+#     # self.bn1 = nn.BatchNorm1d(60)
+#     self.out_layer = nn.Linear(50, 4)
+    
+#   def forward(self, x):
+#     x = F.relu(self.conv1(x))
+#     x = self.bn2d_1(x)
+#     x = self.pool1(x)
+#     x = F.relu(self.conv2(x))
+#     x = self.bn2d_2(x)
+#     x = self.pool2(x)
+#     x = x.view(-1, self.num_flat_features(x))
+#     # print(x)
+#     x = F.relu(self.fc(x))
+#     # x = self.bn1(x)
+#     # print("After Hideen Layer",x)
+#     x = self.out_layer(x)
+#     # print("After Output layer",x)
+#     # x = F.log_softmax(x, dim=1)
+#     # x = F.softmax(x,dim=1) # for cross entropyloss don't do softmax as it internally does it
+#     # print(x)
+#     return x
+
+#   def num_flat_features(self, x):
+#     size = x.size()[1:]  # all dimensions except the batch dimension
+#     num_features = 1
+#     for s in size:       # Get the products
+#       num_features *= s
+#     return num_features
+
+class Net(nn.Module):
+  def __init__(self, num_classes=10):
+    super(Net, self).__init__()
+    self.layer1 = nn.Sequential(
+        nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+        nn.BatchNorm2d(16),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2))
+    self.layer2 = nn.Sequential(
+        nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+        nn.BatchNorm2d(32),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2))
+    self.fc = nn.Linear(4608, 4)
+      
+  def forward(self, x):
+    out = self.layer1(x)
+    out = self.layer2(out)
+    out = out.reshape(out.size(0), -1)
+    out = self.fc(out)
+    return out
---
+++ Assignments/4/WhiteBackground/ArchiveModel/MusicController.py
@@ -0,0 +1,56 @@
+from imutils.video import count_frames
+import numpy as np
+import cv2
+import sys
+import os
+from network import *
+
+'''
+  0 : BackGround
+  1 : Next
+  2 : Prev
+  3 : Stop
+'''
+
+labels = [0,1,2,3]
+
+PATH = "./Models/SimpleMusicModel_84.model"
+
+net = Net()
+net.load_state_dict(torch.load(PATH))
+net.eval()
+
+cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(sys.argv[1])
+# Check if the webcam is opened correctly
+if not cap.isOpened():
+  raise IOError("Cannot open webcam")
+
+classes = ["Background","Next","Prev","Stop"]
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+with torch.no_grad():
+  while True:
+    ret, frame = cap.read()
+    img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    img = cv2.medianBlur(img,3)
+    edges = cv2.Canny(img,70,140)
+    iedges = cv2.resize(edges, (50,50), interpolation = cv2.INTER_AREA)
+    iedges = iedges/255.0
+    iframe = iedges
+    inp = Variable(torch.from_numpy(iframe.reshape(1,1,iframe.shape[0],iframe.shape[1])).float())
+    outputs = net(inp)
+    _, predicted = torch.max(outputs.data, 1)
+    outputs = F.softmax(net(inp),dim=1) # when using cross entropy as outputs are logits
+    print(outputs)
+    lab = classes[predicted]
+    frame = cv2.putText(frame,lab,(250,250), font, .9,(255,2,5),2,cv2.LINE_AA)
+    cv2.imshow('WebCam', np.hstack([frame,cv2.merge([edges]*3)]))
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
